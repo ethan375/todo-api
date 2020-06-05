69b95fb05cdce27ec6a0d1267f5b688cbf6ecdf4
ethan375 <ethanebel1@gmail.com>
2020-06-05 10:11:37 -0600

    created route for creating new tasks. new tasks are saved to their appropriate lists

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const task = require('../models/Tasks.js');
+const list = require('../models/Lists')
 
-router.get('/', (req, res)=> {
-    task.find({}, (err, foundTasks) => {
+router.get('/:id', (req, res)=> {
+    task.findOne({id: req.param.id}, (err, foundTasks) => {
         if (err) {
             console.error(err);
         } else {
             res.send(foundTasks);
+        }
+    })
+})
+
+
+router.post('/new', (req, res) => {
+    const newTask = new task({
+        name: req.body.name,
+        list: req.body.list
+    });
+
+    newTask.save(newTask, (err, createdTask) => {
+        if (err) {
+            console.error(err)
+            res.send(`There was an issue creating the new task`)
+        } else {
+            // res.send(createdTask)
+        }
+    })
+
+    list.findById(req.body.list, (err, foundList) => {
+        if (err) {
+            console.error(err)
+            res.send(`There was a problem saving the task to the list`)
+        } else {
+            foundList.tasks.push(newTask)
+            foundList.save()
+            res.send(newTask)
         }
     })
 })
