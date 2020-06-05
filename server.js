e025f06748ea5ae9135ec9e9699d6525a74e4df8
ethan375 <ethanebel1@gmail.com>
2020-06-04 18:22:30 -0600

    configured cors settings to allow cross origin access

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const methodOverride = require('method-override');
+const cors = require('cors');
 
 require('dotenv').config()
 
@@ -20,6 +21,8 @@ app.use(express.json())
 app.use(express.static('public'));
 
 app.use(methodOverride('_method'));
+
+app.use(cors())
 
 // app.use(session({
 //     secret: process.env.SECRET_KEY,
