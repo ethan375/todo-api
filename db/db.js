4d46b1dfc5732f89842c43375e954be6f9374ec5
ethan375 <ethanebel1@gmail.com>
2020-07-17 12:06:31 -0600

    added production db to db connect file

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || process.env.DB_HOST, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || process.env.PRODUCTION_MONGO, {useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.connection.on('connected', () => {
     console.log(`connected to the db`)
