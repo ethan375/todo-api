8e51c69045ee4c7dd4707f6af0609eaad3a796e9
ethan375 <ethanebel1@gmail.com>
2020-11-17 22:27:38 -0700

    create a user

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,21 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 router.post('create-user', ( req, res ) => {
+    let body = req.body
     
+    let newUser = new User({
+        username: body.username,
+        password: body.password,
+        lists: body.lists,
+    })
+
+    newUser.save( newUser, (err, createdUser) => {
+        if ( err ) {
+            console.error(err)
+        } else {
+            res.send(createdUser)
+        }
+    })
 })
 
 
